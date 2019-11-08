8a5ab2f5dbdba0f06a617c36555a3984530a0bcf
Steve Halladay <steve.halladay@datastax.com>
2019-11-08 17:35:31 +0000

    Updated to use the current driver

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,10 +1,13 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class DBConnection {
     private static String connectionPath = "/home/ubuntu/workspace/creds.zip";
     private static String username = "KVUser";
     private static String password = "KVPassword";
 
-    public static String getConnectionPath() {
-        return connectionPath;
+    public static Path getConnectionPath() {
+        return Paths.get(connectionPath);
     }
 
     public static String getUsername() {
